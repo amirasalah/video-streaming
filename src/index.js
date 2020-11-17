df25c4b4e4d9e49f04e4c6a631fa251c4f8a5e75
amirasalah <amera.salah.89@gmail.com>
2020-11-17 11:03:27 +0200

    adding global styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './app'
+import 'normalize.css'
+import { GlobalStyle } from './globalStyles'
 
-render(<App />, document.getElementById('root'))
+render(
+    <>
+        <GlobalStyle />
+        <App />
+    </>,
+    document.getElementById('root'),
+)
