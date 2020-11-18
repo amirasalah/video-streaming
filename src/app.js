48e33aeb0827ddd2788e55a8cbafc931cd31bd8c
amirasalah <amera.salah.89@gmail.com>
2020-11-18 18:26:20 +0200

    add new pages

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import * as ROUTES from './constants/routes'
-import Home from './pages/home'
+import { Home, Browse, SignIn, SignUp } from './pages'
 
 const App = () => {
     return (
         <Router>
             <Route exact path={ROUTES.HOME}>
                 <Home />
+            </Route>
+            <Route exact path={ROUTES.BROWSE}>
+                <Browse />
+            </Route>
+            <Route exact path={ROUTES.SIGN_IN}>
+                <SignIn />
+            </Route>
+            <Route exact path={ROUTES.SIGN_UP}>
+                <SignUp />
             </Route>
         </Router>
     )
