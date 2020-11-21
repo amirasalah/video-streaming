2e3babfb46bd64498c36b4913e3bc2e1c2344dfa
amirasalah <amera.salah.89@gmail.com>
2020-11-21 21:26:00 +0200

    who is watching

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,7 +1,27 @@
 import React from 'react'
 import SelectProfileContainer from './profiles'
+import { FireBaseContext } from '../context/firebase'
+import { Loading } from '../components'
 
 const BrowseContainer = ({ slides }) => {
-    return <SelectProfileContainer></SelectProfileContainer>
+    const [profile, setProfile] = React.useState({})
+    const [loading, setLoading] = React.useState(true)
+    const { firebase } = React.useContext(FireBaseContext)
+    const user = firebase.auth().currentUser || {}
+
+    React.useEffect(() => {
+        setTimeout(() => {
+            setLoading(false)
+        }, 3000)
+    }, [profile.displayName])
+
+    // return profile.displayName ? (
+    //     loading ? (
+    //         <Loading src={user.photoURL} />
+    //     ) : null
+    // ) : (
+    //     <SelectProfileContainer user={user} setProfile={setProfile} />
+    // )
+    return <SelectProfileContainer user={user} setProfile={setProfile} />
 }
 export default BrowseContainer
--- src/containers/profiles.js
+++ src/containers/profiles.js
@@ -1,18 +1,38 @@
 import React from 'react'
 import { Header } from '../components'
+import { Profiles } from '../components'
+
 import * as ROUTES from '../constants/routes'
 
-const SelectProfileContainer = ({ user }) => {
+const SelectProfileContainer = ({ user, setProfile }) => {
     return (
-        <Header bg={false}>
-            <Header.Frame>
-                <Header.Logo
-                    src='images/logo.svg'
-                    to={ROUTES.HOME}
-                    alt='Netflix'
-                />
-            </Header.Frame>
-        </Header>
+        <>
+            <Header bg={false}>
+                <Header.Frame>
+                    <Header.Logo
+                        src='images/logo.svg'
+                        to={ROUTES.HOME}
+                        alt='Netflix'
+                    />
+                </Header.Frame>
+            </Header>
+            <Profiles>
+                <Profiles.Title>Who's watching?</Profiles.Title>
+                <Profiles.List>
+                    <Profiles.User
+                        onClick={() =>
+                            setProfile({
+                                displayName: user.displayName,
+                                photoURL: user.photoURL,
+                            })
+                        }
+                    >
+                        <Profiles.Picture src={user.photoURL} />
+                        <Profiles.Name>{user.displayName}</Profiles.Name>
+                    </Profiles.User>
+                </Profiles.List>
+            </Profiles>
+        </>
     )
 }
 export default SelectProfileContainer
