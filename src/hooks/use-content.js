8cc8cebf0a96a53e38a56572e13facc19564f3c8
amirasalah <amera.salah.89@gmail.com>
2020-11-21 16:30:09 +0200

    custom hook to retrieve data

---
+++ src/hooks/use-content.js
@@ -0,0 +1,31 @@
+import { useState, useContext, useEffect } from 'react'
+import { FireBaseContext } from '../context/firebase'
+
+const useContent = target => {
+    const [content, setContent] = useState([])
+    const { firebase } = useContext(FireBaseContext)
+
+    useEffect(() => {
+        firebase
+            .firestore()
+            .collection(target)
+            .get()
+            .then(snapshot => {
+                const allContent = snapshot.docs.map(contentObj => {
+                    return {
+                        ...contentObj.data(),
+                        docId: contentObj.id,
+                    }
+                })
+                setContent(allContent)
+            })
+            .catch(err => {
+                console.log(err.message)
+            })
+    }, [target])
+    return {
+        [target]: content,
+    }
+}
+
+export default useContent
--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useContent } from '../hooks'
 
 const Browse = () => {
+    const { films } = useContent('films')
+    const { series } = useContent('series')
+
+    console.log(films)
     return <div>Browse</div>
 }
 export default Browse
